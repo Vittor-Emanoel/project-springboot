9f8c7dacc37d7ee95b3ad355dba9e974a680737b
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-05 23:06:33 -0300

    feat: init project

---
+++ src/main/java/br/com/vittor/projectspringboot/ProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.vittor.projectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootApplication.class, args);
+	}
+
+}
